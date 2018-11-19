9dcee79b69df11ce5b05c3f5532b2480f6fd4a9b
Mckinsey666 <whereisbrianchao520@gmail.com>
2018-11-19 15:33:48 +0800

    UVa_514 sol

---
+++ STL/sol/UVa_514.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+bool can_reorder(queue<int>after){
+    stack<int>s;
+    int n = after.size();
+    for(int i = 1; i <= n; ++i){
+        s.push(i);
+        while(!s.empty() && s.top() == after.front()){
+            after.pop(); s.pop();
+        }
+    }
+    return s.empty();
+}
+
+int main(){
+    int n;
+    while(cin >> n && n){
+        while(1){
+            queue<int>after;
+            int t;
+            cin >> t; after.push(t);
+            if(!t) break;
+            for(int i = 1; i < n; ++i) {
+                cin >> t;
+                after.push(t);
+            }
+            cout << ((can_reorder(after)) ? "Yes\n" : "No\n");
+        }
+        cout << '\n';
+    }
+    return 0;
+}
